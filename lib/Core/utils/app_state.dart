4ea75b337eef2c3bb5960b3d72c93c3f088e06b9
Omar_Ameer <omar.ameer244@gmail.com>
2023-12-19 18:56:11 +0200

    Updating the IP Address.

--- lib/Core/utils/app_state.dart
+++ lib/Core/utils/app_state.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
-  String _baseUrl = 'https://mohammedsamy.pythonanywhere.com/';
-  // Old One: http://10.0.2.2:5000
+  String _baseUrl = 'http://10.0.2.2:5000';
+  // One: http://10.0.2.2:5000
+  // Two: https://mohammedsamy.pythonanywhere.com/
 
   String get baseUrl => _baseUrl;
 
