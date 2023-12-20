196c7c0dcf18bd7299943f9d0b71bc7b5ae73d5b
Omar_Ameer <omar.ameer244@gmail.com>
2023-12-20 15:38:59 +0200

    Added the Confetti Effect.

--- lib/Features/info/views/widgets/team_members_item.dart
+++ lib/Features/info/views/widgets/team_members_item.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ss_algorithms_app/Core/utils/styles.dart';
 import 'package:ss_algorithms_app/constants.dart';
+import 'package:confetti/confetti.dart'; // Import the confetti package
 
-class TeamMembersItem extends StatelessWidget {
+class TeamMembersItem extends StatefulWidget {
   const TeamMembersItem({
     super.key,
     required this.name,
@@ -11,24 +12,62 @@ class TeamMembersItem extends StatelessWidget {
   final String name;
 
   @override
+  // ignore: library_private_types_in_public_api
+  _TeamMembersItemState createState() => _TeamMembersItemState();
+}
+
+class _TeamMembersItemState extends State<TeamMembersItem> {
+  late ConfettiController _confettiController;
+
+  @override
+  void initState() {
+    _confettiController = ConfettiController(
+      duration: const Duration(seconds: 1),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: kPrimaryColor,
-      ),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Text(
-            name,
-            style: Styles.sFProDisplayBold.copyWith(
-              fontSize: 24,
-              color: kButtonColor2,
+    return InkWell(
+      onTap: () {
+        _confettiController.play();
+      },
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: kPrimaryColor,
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Text(
+                  widget.name,
+                  style: Styles.sFProDisplayBold.copyWith(
+                    fontSize: 24,
+                    color: kButtonColor2,
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment.center,
+            child: ConfettiWidget(
+              gravity: 0,
+              confettiController: _confettiController,
+            ),
+          ),
+        ],
       ),
     );
   }
